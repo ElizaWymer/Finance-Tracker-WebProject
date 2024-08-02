abd69f34f85695eefd5cdaf7507b6536fdb0546e
Elizabeth <elizabethwymer2004@gmail.com>
2024-08-02 17:53:31 +0100

    Added modules folder

--- src/Message.tsx
+++ src/Message.tsx
@@ -2,7 +2,7 @@ function Message()
 {
     const name = "Elizabeth";
     if (name)
-        return <h1>            I'm {name}!!! </h1>;
+        return <h1>I'm {name}!!! </h1>;
     return <h1> Hello World!</h1>;
 }
 

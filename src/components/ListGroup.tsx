2ebd950956d1db517470ced95ff75edf80b42383
Elizabeth <elizabethwymer2004@gmail.com>
2024-08-02 19:05:45 +0100

    Improved ListGroup component by making it dynamic

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,16 @@
+import { Fragment } from "react/jsx-runtime";
+const fruits = ["apple", "banana", "cherry", "durian", "stawberry"];
+
 function ListGroup() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <Fragment>
+      <h2> List Heading</h2>
+      <ul className="list-group">
+        {fruits.map((fruit) => (
+          <li key={fruit}>{fruit}</li>
+        ))}
+      </ul>
+    </Fragment>
   );
 }
 

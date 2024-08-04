81a0803e231745d43654c7ce68b5d21805b04ea2
Elizabeth <elizabethwymer2004@gmail.com>
2024-08-05 00:13:40 +0100

    Added state management features for buttons

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,42 @@
+import { MouseEvent, useState } from "react";
 import { Fragment } from "react/jsx-runtime";
-const fruits = ["apple", "banana", "cherry", "durian", "stawberry"];
 
 function ListGroup() {
+  let fruits = ["Apples", "Bananas", "Cherrys", "Durians", "Strawberries"];
+  //fruits = [];
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  const message = fruits.length === 0 && <p>The fruit bowl is empty :(</p>;
+  /*
+  const getMessage = () => {
+    return fruits.length === 0 ? <p>The fruit bowl is empty :(</p> : null;
+  };
+  */
+
+  const handleClick = (event: MouseEvent) => console.log(event);
+
   return (
     <Fragment>
-      <h2> List Heading</h2>
+      <h2>Fruit Bowl</h2>
+      {message}
       <ul className="list-group">
-        {fruits.map((fruit) => (
-          <li key={fruit}>{fruit}</li>
+        {fruits.map((fruit, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={fruit}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+            //{handleClick}
+            //{() => console.log("Clicked " + fruit, index)}
+          >
+            {fruit}
+          </li>
         ))}
       </ul>
     </Fragment>

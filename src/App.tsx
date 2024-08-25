fe180a846a0ca9a22af191bd4c8eb4449bfc7a48
Elizabeth <elizabethwymer2004@gmail.com>
2024-08-25 12:15:03 +0100

    Used an icon component and customised it

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
-import Message from "./Message";
-import ListGroup from "./components/ListGroup/index";
+import { FaCat } from "react-icons/fa";
 
 function App() {
-  let fruits = ["Apples", "Bananas", "Cherrys", "Durians", "Strawberries"];
-  //fruits = [];
-  //let cities = ["Leeds", "Wakefield", "Huddersfield"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <Message />{" "}
-      <ListGroup
-        items={fruits}
-        heading="Fruit Bowl"
-        onSelectItem={handleSelectItem}
-      />
+      <FaCat color="blue" size="50" />
     </div>
   );
 }

d47ab508769f06de7f13423d3f4a25b4f2fb12e5
Elizabeth <elizabethwymer2004@gmail.com>
2024-08-25 18:45:34 +0100

    Created a state hook object with multiple properties

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import LikeComponent from "./components/LikeComponent/index";
+import { useState } from "react";
 
 function App() {
+  const [game, setGame] = useState({
+    id: 0,
+    player: { name: "Placeholder", highscore: 0 },
+  });
+
+  const handleClick = () => {
+    setGame({
+      ...game,
+      id: 1,
+      player: { ...game.player, name: "Elizablobs", highscore: 69 },
+    });
+  };
+
   return (
     <div>
-      <LikeComponent />
+      <button
+        onClick={() => {
+          handleClick();
+        }}
+      >
+        Load Player Data
+      </button>
+      {game.id} {game.player.name} {game.player.highscore}
     </div>
   );
 }
---
+++ src/components/Message.tsx
@@ -0,0 +1,7 @@
+function Message() {
+  const name = "Elizabeth";
+  if (name) return <h1>I'm {name}!!! </h1>;
+  return <h1> Hello World!</h1>;
+}
+
+export default Message;

444549ae1ff38a8c037ac9ae00c6b98cb1c9478a
Elizabeth <elizabethwymer2004@gmail.com>
2024-08-27 14:26:41 +0100

    Simplified updating objects using immer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import { useState } from "react";
+import produce from "immer";
 
 function App() {
-  const [moods, setMood] = useState(["Happy", "Sad", "Angry"]);
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
+  ]);
 
   const handleClick = () => {
-    setMood([...moods, "Fear"]);
-    console.log([...moods, "Fear"]);
-    setMood(moods.filter((mood) => mood !== "Angry"));
-    console.log(moods.filter((mood) => mood !== "Angry"));
-    setMood(moods.map((mood) => (mood === "Happy" ? "Joy" : mood)));
-    console.log(moods.map((mood) => (mood === "Happy" ? "Joy" : mood)));
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
   };
 
   return (
     <div>
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title}: {bug.fixed ? "Fixed" : "New"}
+        </p>
+      ))}
       <button
         onClick={() => {
           handleClick();
         }}
-      ></button>
+      >
+        Click Me
+      </button>
     </div>
   );
 }

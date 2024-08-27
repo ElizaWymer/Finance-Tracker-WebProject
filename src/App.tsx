8b47abcd3f34b33fcdb71903a74624ad4c2f0a05
Elizabeth <elizabethwymer2004@gmail.com>
2024-08-27 16:50:59 +0100

    Created an extendable text component using a statehook

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import { useState } from "react";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
-  const [drink, setDrink] = useState({ name: "Hot Chocolate", price: 3 });
-
-  const handleClick = () => {
-    setDrink({
-      ...drink,
-      //price: 3.75,
-      price: drink.price + 1,
-    });
-  };
-
   return (
     <div>
-      {drink.name}: £{drink.price}
-      <br></br> <br></br>
-      <button onClick={handleClick}> Equate for Inflation</button>
+      <ExpandableText>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi qui non
+        rerum dolorem id, illo culpa fuga excepturi voluptatibus dicta quae quia
+        consectetur sint consequuntur! Veniam modi, nostrum asperiores
+        dignissimos optio tenetur, ab neque minus corporis, officiis ipsam eius.
+        Autem a omnis, beatae cum mollitia praesentium ullam eaque aliquid at ad
+        facere harum modi fugit vel. Eveniet ratione deserunt deleniti nisi nemo
+        odit, perferendis recusandae, magnam labore tempore sapiente, voluptatum
+        eligendi minus qui culpa nihil quibusdam. Doloremque, enim cum! Ipsa
+        alias exercitationem ullam quis, iste voluptate quaerat quidem fugiat
+        suscipit rem nisi iure eaque saepe voluptas blanditiis molestiae aut
+        similique.
+      </ExpandableText>
     </div>
   );
 }

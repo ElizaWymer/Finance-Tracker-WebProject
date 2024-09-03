f998098cb0c3a776aa1163f5f247420a0a149203
Elizabeth <elizabethwymer2004@gmail.com>
2024-09-03 14:25:12 +0100

    Created a table that can display and delete expense data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import Form from "./components/Form";
+import { useState } from "react";
+import ExpenseList from "./Expense Tracker/ExpenseList";
 
 function App() {
+  const [expenses, setExpense] = useState([
+    { id: 1, description: "foo", amount: 10, category: "Utilities" },
+    { id: 2, description: "bar", amount: 12.5, category: "test" },
+    { id: 3, description: "coo", amount: 4, category: "Futilities" },
+    { id: 4, description: "dar", amount: 9.7483, category: "notatest" },
+  ]);
+
   return (
     <div>
-      <Form></Form>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpense(expenses.filter((e) => e.id !== id))}
+      ></ExpenseList>
     </div>
   );
 }

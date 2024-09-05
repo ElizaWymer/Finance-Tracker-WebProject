9977aefecd42463d59e28c09bd49244a05f8ad80
Elizabeth <elizabethwymer2004@gmail.com>
2024-09-05 17:23:35 +0100

    Created a table that displays a list of all items purchased as well as a button that allows the items to be filtered by type

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
 import { useState } from "react";
-import ExpenseList from "./Expense Tracker/ExpenseList";
+import ExpenseList from "./Expense Tracker/Components/ExpenseTable";
+import ExpenseFilter from "./Expense Tracker/Components/ExpenseFilter";
 
 function App() {
   const [expenses, setExpense] = useState([
-    { id: 1, description: "foo", amount: 10, category: "Utilities" },
-    { id: 2, description: "bar", amount: 12.5, category: "test" },
-    { id: 3, description: "coo", amount: 4, category: "Futilities" },
-    { id: 4, description: "dar", amount: 9.7483, category: "notatest" },
+    { id: 1, description: "Garlic Bread", amount: 10, category: "Groceries" },
+    {
+      id: 2,
+      description: "Into the Spider-Verse DVD",
+      amount: 12.5,
+      category: "Entertainment",
+    },
+    { id: 3, description: "Sledgehammer", amount: 4, category: "Utilities" },
+    {
+      id: 4,
+      description: "Alien Isolation",
+      amount: 9.7483,
+      category: "Entertainment",
+    },
   ]);
+
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const visableExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
 
   return (
     <div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        ></ExpenseFilter>
+      </div>
+
       <ExpenseList
-        expenses={expenses}
+        expenses={visableExpenses}
         onDelete={(id) => setExpense(expenses.filter((e) => e.id !== id))}
       ></ExpenseList>
     </div>
